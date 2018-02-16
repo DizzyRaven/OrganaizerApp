440ee3276cc3ae03ae672132ee041faa33dc2a4b
DizzyRaven <akaynt47@gmail.com>
2018-02-16 18:29:00 +0200

    Just masterDetailPage

--- OrganaizerApp/OrganaizerApp/MainPage.xaml.cs
+++ OrganaizerApp/OrganaizerApp/MainPage.xaml.cs
@@ -7,11 +7,24 @@ using Xamarin.Forms;
 
 namespace OrganaizerApp
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+
+            Detail = new NavigationPage(new MainContentPage());
+            IsPresented = false;
 		}
-	}
+        private void btn_mainPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new MainContentPage());
+            IsPresented = false;
+        }
+        private void btn_settingsPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new SettingsContentPage());
+            IsPresented = false;
+        }
+    }
 }
